7e0d04bdbf2e1664a445a50e0e0e8b835b231b14
Ioannis Fytros <rinenweb@gmail.com>
2026-03-05 00:31:19 +0200

    Update indicator_registry.py

--- models/indicator_registry.py
+++ models/indicator_registry.py
@@ -1,10 +1,9 @@
-
 from indicators import i206, i214, i234, i273, i204
 
 INDICATORS = {
-    "206": {"name": "Internal search mechanism", "module": i206, "default": True},
-    "214": {"name": "Contact details", "module": i214, "default": True},
-    "234": {"name": "Privacy policy", "module": i234, "default": True},
-    "273": {"name": "Social networking features", "module": i273, "default": True},
-    "204": {"name": "Mobile device accessibility", "module": i204, "default": True},
+    "206": {"name": "Internal search mechanism", "module": i206},
+    "214": {"name": "Contact details", "module": i214},
+    "234": {"name": "Privacy policy", "module": i234, "config": i234.CONFIG},
+    "273": {"name": "Social networking features", "module": i273},
+    "204": {"name": "Mobile device accessibility", "module": i204},
 }
