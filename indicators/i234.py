478f2824d585b83a5562345b135008560f9fcf16
Ioannis Fytros <rinenweb@gmail.com>
2026-03-05 00:27:13 +0200

    Update i234.py

--- indicators/i234.py
+++ indicators/i234.py
@@ -1,10 +1,24 @@
-
-def run(soup, url):
+CONFIG = {
+    "type": "keyword",
+    "keywords_default": [
+        "privacy policy",
+        "πολιτική απορρήτου"
+    ],
+    "case_insensitive": True,
+    "accent_insensitive": True
+}
+from scanner.html_utils import normalize_text
+def run(soup, url, keywords=None):
     if not soup:
         return 0, None
+    if not keywords:
+        keywords = CONFIG["keywords_default"]
+
+    keywords = [normalize_text(k) for k in keywords]
 
     for a in soup.find_all("a", href=True):
-        if "privacy" in a.text.lower():
-            return 1, a.get("href")
-
+        text = normalize_text(a.text)
+        for kw in keywords:
+            if kw in text:
+                return 1, a["href"]
     return 0, None
