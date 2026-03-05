d3d63a836ef712d6f2d379d0055b30cd3f5d2839
Ioannis Fytros <rinenweb@gmail.com>
2026-03-05 22:03:18 +0200

    Update i273.py to implement crawling function

--- indicators/i273.py
+++ indicators/i273.py
@@ -1,16 +1,16 @@
 CONFIG = {
     "name": "Social networking features",
     "description": "Presence of links to social networking platforms such as Facebook, Twitter, YouTube or Instagram."
+    "crawl": True,
 }
 SOCIAL = ["facebook.com","twitter.com","youtube.com","instagram.com","linkedin.com"]
 
-def run(soup, url, **kwargs):
-    if not soup:
-        return 0, None
+def run(pages, url, **kwargs):
 
-    for a in soup.find_all("a", href=True):
-        for s in SOCIAL:
-            if s in a["href"]:
-                return 1, a["href"]
-
+    for p in pages:
+        soup = p["soup"]
+        for a in soup.find_all("a", href=True):
+            for s in SOCIAL:
+                if s in a["href"]:
+                    return 1, a["href"]
     return 0, None
