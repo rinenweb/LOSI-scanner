ad4f6fef694eb3c4bd3c75d67084a7fe52daa18f
Ioannis Fytros <rinenweb@gmail.com>
2026-03-05 22:02:33 +0200

    Update i234.py to implement crawling function

--- indicators/i234.py
+++ indicators/i234.py
@@ -2,6 +2,7 @@ CONFIG = {
     "name": "Privacy policy",
     "description": "Existence of a privacy policy or statement available on the municipal government portal.",
     "type": "keyword",
+    "crawl": True,
     "keywords_default": [
         "privacy policy",
         "πολιτική απορρήτου"
@@ -10,17 +11,17 @@ CONFIG = {
     "accent_insensitive": True
 }
 from scanner.html_utils import normalize_text
-def run(soup, url, keywords=None, **kwargs):
-    if not soup:
-        return 0, None
+def run(pages, url, keywords=None, **kwargs):
+
     if not keywords:
         keywords = CONFIG["keywords_default"]
-
     keywords = [normalize_text(k) for k in keywords]
 
-    for a in soup.find_all("a", href=True):
-        text = normalize_text(a.text)
-        for kw in keywords:
-            if kw in text:
-                return 1, a["href"]
+    for p in pages:
+        soup = p["soup"]
+        for a in soup.find_all("a", href=True):
+            text = normalize_text(a.text)
+            for kw in keywords:
+                if kw in text:
+                    return 1, a["href"]
     return 0, None
