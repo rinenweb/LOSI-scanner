24cc63f7d85f452f7787c5a60a194f56f982856c
Ioannis Fytros <rinenweb@gmail.com>
2026-03-05 22:01:32 +0200

    Update i214.py  to implement crawling function

--- indicators/i214.py
+++ indicators/i214.py
@@ -1,22 +1,17 @@
 CONFIG = {
     "name": "Contact details",
     "description": "Evidence of a 'contact us' feature including phone numbers, email addresses, physical addresses or contact forms."
+    "crawl": True
 }
 import re
 
-def run(soup, url, **kwargs):
-    if not soup:
-        return 0, None
-
-    text = soup.get_text().lower()
-
-    email = re.search(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+", text)
-    phone = re.search(r"\+?[0-9][0-9\-\s]{7,}", text)
-
-    if email:
-        return 1, email.group(0)
-
-    if phone:
-        return 1, phone.group(0)
-
+def run(pages, url, **kwargs):
+    for p in pages:
+        text = p["soup"].get_text().lower()
+        email = re.search(r"[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+", text)
+        phone = re.search(r"\+?[0-9][0-9\-\s]{7,}", text)
+        if email:
+            return 1, p["url"]
+        if phone:
+            return 1, p["url"]
     return 0, None
