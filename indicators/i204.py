6e9a104ce3fa13f866c1413fe6b760d198ab751a
Ioannis Fytros <rinenweb@gmail.com>
2026-03-05 21:59:25 +0200

    Update i204.py

--- indicators/i204.py
+++ indicators/i204.py
@@ -1,14 +1,15 @@
 CONFIG = {
     "name": "Mobile device accessibility",
     "description": "The portal displays and works well when accessed through a mobile device (e.g., smartphone or tablet)."
+    "crawl": False
 }
-def run(soup, url, **kwargs):
-    if not soup:
+def run(pages, url, **kwargs):
+    if not pages:
         return 0, None
 
     meta = soup.find("meta", attrs={"name":"viewport"})
 
     if meta:
-        return 1, "viewport meta tag"
+        return 1, pages[0]["url"]
 
     return 0, None
