9d93316a522d9d0bc37feac68ea354931825f2b5
Ioannis Fytros <rinenweb@gmail.com>
2026-03-05 22:00:47 +0200

    Update i206.py  to implement crawling function

--- indicators/i206.py
+++ indicators/i206.py
@@ -1,15 +1,13 @@
 CONFIG = {
     "name": "Internal search mechanism",
     "description": "A search bar on the main web page of the municipal government portal."
+    "crawl": True
 }
-def run(soup, url, **kwargs):
-    if not soup:
-        return 0, None
-
-    if soup.find("input", {"type":"search"}):
-        return 1, "input[type=search]"
-
-    if soup.find("form", {"role":"search"}):
-        return 1, "form[role=search]"
-
+def run(pages, url, **kwargs):
+   for p in pages:
+        soup = p["soup"]
+        if soup.find("input", {"type": "search"}):
+            return 1, p["url"]
+        if soup.find("form", {"role": "search"}):
+            return 1, p["url"]
     return 0, None
