4fca56d8406b73cf3e7f952d2c30a921f32f40fa
Ioannis Fytros <rinenweb@gmail.com>
2026-03-05 22:38:13 +0200

    Update i206.py

--- indicators/i206.py
+++ indicators/i206.py
@@ -4,20 +4,20 @@ CONFIG = {
     "crawl": True
 }
 
+
 def run(pages, url, **kwargs):
 
     for p in pages:
-
         soup = p["soup"]
 
-        # if search input
+        # search input
         if soup.find("input", {"type": "search"}):
             return 1, p["url"]
 
         if soup.find("input", {"name": "s"}):
             return 1, p["url"]
 
-        # if search form
+        # search form
         if soup.find("form", {"role": "search"}):
             return 1, p["url"]
 
@@ -26,19 +26,18 @@ def run(pages, url, **kwargs):
             if "search" in action:
                 return 1, p["url"]
 
-        # if link to search page
+        # search links
         for a in soup.find_all("a", href=True):
-
             href = a["href"].lower()
             text = a.get_text(strip=True).lower()
 
-            if "search" in href or "/search" in href:
+            if "/search" in href or "search" in href:
                 return 1, p["url"]
 
             if "αναζήτηση" in text:
                 return 1, p["url"]
 
-        # if search icon
+        # search icon
         if soup.find(class_="fa-search"):
             return 1, p["url"]
 
